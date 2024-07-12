4db644855db1658f18fe64d07f8a1bee6aec9a4b
yiwang <yiwang0261@qq.com>
2024-07-12 11:30:19 +0800

    项目搭建

---
+++ dao/src/main/java/edu/jsu/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package edu.jsu.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Author: yiwang
+ * @Date: 2024/7/12
+ */
+@Mapper
+public interface UserMapper {
+}
---
+++ pojo/src/main/java/edu/jsu/pojo/User.java
@@ -0,0 +1,17 @@
+package edu.jsu.pojo;
+
+import lombok.*;
+
+/**
+ * @Author: yiwang
+ * @Date: 2024/7/12
+ */
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private String username;
+    private String password;
+}
